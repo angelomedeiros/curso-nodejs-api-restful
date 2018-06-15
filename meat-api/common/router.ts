951765cb22157ce86392758d699ae42e03f57c3a
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-15 09:07:59 -0300

    Aula 31

---
+++ meat-api/common/router.ts
@@ -0,0 +1,49 @@
+import * as restify from 'restify'
+
+export abstract class Router {
+	abstract applyRoutes(application: restify.Server)
+}
+
+// Routes
+
+// this.application.get('/hello', (req, resp, next) => {
+// 	// As duas linhas de baixo(contentType e send) representam o resp.json
+// 	resp.contentType = 'application/json'
+// 	resp.send({ mensagem: 'Hello world!' })
+// 	// resp.json({message: 'hello'})
+
+// 	// Altera o status
+// 	resp.status(200)
+
+// 	// Altera o contentType
+// 	// resp.contentType = 'application/json'
+// 	// resp.setHeader('Content-Type', 'application/json')
+// 	return next()
+// })
+
+// // Obtem os parâmetros passados na url
+
+// this.application.get('/info', [
+// 	(req, resp, next) => {
+// 		if (req.userAgent() && req.userAgent().includes('MSIE 7.0')){
+// 			// resp.status(400)
+// 			// resp.json({ message: 'Por favor, atualize seu browser' })
+// 			// return next(false)
+// 			let error: any = new Error()
+// 			error.statusCode = 400
+// 			error.message = 'Por favor pnc, tome vergonha!'
+// 			return next(error)
+// 		}
+// 		return next()
+// 	},
+// 	(req, resp, next) => {
+// 		resp.json({
+// 			browser: req.userAgent(),
+// 			method: req.method,
+// 			url: req.url,
+// 			href: req.href(),
+// 			path: req.path(),
+// 			query: req.query
+// 	})
+// 	return next()
+// }])
---
+++ meat-api/users/users.model.ts
@@ -0,0 +1,44 @@
+// const users = [
+// 	{id: '1', name: 'Angelo Medeiros', email: 'angelo@email.com'},
+// 	{id: '2', name: 'Thalita Oliverira', email: 'thalita@email.com'}
+// ]
+
+// export class User {
+// 	static findAll(): Promise<any[]>{
+// 		return Promise.resolve(users)	
+// 	}
+// 	static findById(id: string): Promise<any>{
+// 		return new Promise(resolve =>{
+// 			const filtered = users.filter(user => user.id === id)
+// 			let user = undefined
+// 			if (filtered.length > 0){
+// 				user = filtered[0]
+// 			}
+// 			resolve(user)
+// 		})	
+// 	}
+// }
+
+import * as mongoose from 'mongoose'
+
+export interface User extends mongoose.Document {
+	name: string,
+	email: string,
+	password: string
+}
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String
+	},
+	email: {
+		type: String,
+		unique: true
+	},
+	password: {
+		type: String,
+		select: false
+	}
+})
+
+export const User = mongoose.model<User>('User', userSchema)
