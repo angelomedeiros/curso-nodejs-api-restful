60c315e347b5177cdaff13b3578f53783a99965d
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-15 12:17:59 -0300

    Tratamento de excecoes

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -14,11 +14,15 @@ class UsersRouter extends Router {
 
 		application.get('/users', (req, resp, next) => {
 			// resp.json({ message: 'users router' })
-			User.find().then(this.render(resp, next))
+			User.find()
+				.then(this.render(resp, next))
+				.catch(next)
 		})
 
 		application.get('/users/:id', (req, resp, next) => {
-			User.findById(req.params.id).then(this.render(resp, next))
+			User.findById(req.params.id)
+				.then(this.render(resp, next))
+				.catch(next)
 		})
 
 		application.post('/users', (req, resp, next) => {
@@ -28,7 +32,9 @@ class UsersRouter extends Router {
 			// user.name = req.body.name
 			// user.email = req.body.email
 			// user.password = req.body.password
-			user.save().then(this.render(resp, next))
+			user.save()
+				.then(this.render(resp, next))
+				.catch(next)
 		})
 
 		application.put('/users/:id', (req, resp, next) => {
@@ -41,13 +47,14 @@ class UsersRouter extends Router {
 					resp.send(404)
 				}
 			}).then(this.render(resp, next))
+				.catch(next)
 		})
 
 		application.patch('/users/:id', (req, resp, next) => {
 			const options = { new: true }
 			User.findByIdAndUpdate(req.params.id, req.body, options).then(
 				this.render(resp, next)
-			)
+			).catch(next)
 		})
 
 		application.del('/users/:id', (req, resp, next) => {
@@ -58,7 +65,7 @@ class UsersRouter extends Router {
 					resp.send(404)
 				}
 				return next()
-			})
+			}).catch(next)
 		})
 
 	}
