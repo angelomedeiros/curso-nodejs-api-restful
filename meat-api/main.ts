847688f0c83c917962d7ee2500eb73c65a651bef
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-16 13:23:32 -0300

    Summary:
    - Refatorando o codigo;
    - Criacao das rotas de Restaurants

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,9 +1,14 @@
+import { restaurantsRouter } from './restaurants/restaurants.router'
 import { Server } from './server/server'
 import { usersRouter } from './users/users.router'
 
 const server = new Server()
-server.bootstrap([usersRouter]).then(server => {
-	console.log('Server is listening on:', server.application.address())
+
+server.bootstrap([
+	usersRouter,
+	restaurantsRouter,
+	]).then( address => {
+	console.log('Server is listening on:', address.application.address())
 }).catch(error => {
 	console.log('Server failed to start')
 	console.error(error)
---
+++ meat-api/restaurants/restaurants.router.ts
@@ -0,0 +1,52 @@
+import { Server } 			 from 'restify'
+import { NotFoundError } from 'restify-errors'
+import { ModelRouter } 	 from '../common/model-router'
+import { Restaurant }    from './restaurants.model'
+
+class RestaurantsRouter extends ModelRouter<Restaurant> {
+	constructor() {
+		super(Restaurant)
+	}
+
+	public applyRoutes(application: Server) {
+		application.get('/restaurants', this.findAll)
+		application.get('/restaurants/:id', [this.validadeId, this.findById])
+		application.post('/restaurants', this.save)
+		application.put('/restaurants/:id', [this.validadeId, this.replace])
+		application.patch('/restaurants/:id', [this.validadeId, this.replace])
+		application.del('/restaurants/:id', [this.validadeId, this.delete])
+
+		application.get('/restaurants/:id/menu', [this.validadeId, this.findMenu])
+		application.put('/restaurants/:id/menu', [this.validadeId, this.replaceMenu])
+	}
+
+	private findMenu = (req, resp, next) => {
+		Restaurant.findById(req.params.id, '+menu')
+			.then(rest => {
+				if (!rest) {
+					throw new NotFoundError('Restaurente não encontrado')
+				} else {
+					resp.json(rest.menu)
+					return next()
+				}
+			}).catch(next)
+	}
+
+	private replaceMenu = (req, resp, next) => {
+		Restaurant.findById(req.params.id)
+			.then(rest => {
+				if (!rest) {
+					throw new NotFoundError('Restaurente não encontrado')
+				} else {
+					rest.menu = req.body // Array de menuitem
+					return rest.save()
+				}
+			}).then(rest => {
+				resp.json(rest.menu)
+				return next()
+			}).catch(next)
+	}
+
+}
+
+export const restaurantsRouter = new RestaurantsRouter()
